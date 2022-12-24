d21473e81f7b8b33785939509964e95e26d2ceac
SmallPeenwow <bowdencamryn@gmail.com>
2022-12-24 14:27:32 +0200

    Creating The Board Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { useEffect } from 'react';
+import Board from './components/Board';
+import { updateBoard } from './store';
+import { useAppDispatch, useAppSelector } from './store/hooks';
+import { createBoard } from './utils/createBoard';
 
 function App() {
-	return <h1>HI</h1>;
+	const dispatch = useAppDispatch();
+
+	const board = useAppSelector(({ candyCrush: { board } }) => board);
+	const boardSize = useAppSelector(({ candyCrush: { boardSize } }) => boardSize);
+
+	useEffect(() => {
+		dispatch(updateBoard(createBoard(boardSize)));
+	}, [boardSize, dispatch]);
+
+	return (
+		<div className='flex items-center justify-center h-screen'>
+			<Board />
+		</div>
+	);
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js, jsx, ts, tsx}'],
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	mode: 'jit',
 	theme: {
 		extend: {},
 	},
